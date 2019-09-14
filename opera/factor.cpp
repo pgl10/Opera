3808b250266565177b6b8962f9fa2a43dacdebbe
Pierre <pgl10@yahoo.com>
2019-09-14 16:31:37 +0200

    factor

--- opera/factor.cpp
+++ opera/factor.cpp
@@ -13,8 +13,9 @@ Integer get_prime(Integer n, int a, Integer x) {
       else q = (x-y)%n;
       g = gcd(n, q); 
       if(g > 1 && g < n) { 
+         // ici : g divise n
          if(isprime(g)) return g; 
-         else return get_prime(g, a, s); 
+         else return get_factor(g); 
       } 
    }while(x != y);
    return 0;
@@ -28,15 +29,18 @@ Integer get_factor(Integer n) {
    if(n%3 == 0) return 3; 
    if(n%5 == 0) return 5; 
    if(n%7 == 0) return 7;
-   // on choisit : f(x) = (x^2) + a 
-   // avec a = 1, 3, 5 ou 7 et x0 = 2
+   if(n%11 == 0) return 11;
+   if(n%13 == 0) return 13;
+   if(isprime(n)) return n;
+   // on choisit : f(x) = (x^2) + c
+   // avec x0 = 2 et c = 1, 3, ... , 13
    Integer p, x = 2;
    int a = 1, i = 0;
    do {
       p = get_prime(n, a+i, x);
       if(p > 0) return p;
       i = i+2;
-   }while(i < 7);
+   }while(i < 13);
    // en cas d'échec
    return 0;
 }
